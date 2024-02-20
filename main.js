fcf99fda498d198c06891e27f232f2cf104ae35f
Sachin Rawat <98683079+sachinrwt@users.noreply.github.com>
2024-02-21 02:02:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 window.onscroll = function() {myFunction()};
   
 var navlist = document.getElementById("main-nav");
@@ -39,3 +40,56 @@ var icon = document.getElementById("icon");
         child.classList.remove('is-dimmed');
       })
     }
+
+    // Text effect
+    
+    
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.getElementById("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    const sd = document.getElementById("shortdiscription");
+      sd.remove();
+      const im = document.getElementById("iam");
+      im.add();
+  }
+  else{
+    const im = document.getElementById("iam");
+      im.remove();
+    const sd = document.getElementById("shortdiscription");
+      sd.add();
+  }
+  
+}
+
+var x = window.matchMedia("(max-width: 600px)")
+
+myFunction(x);
